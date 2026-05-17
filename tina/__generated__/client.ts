fb2f282cf9a9731da686b76962d3356231541050
DevComeThru <dev@comethru.com>
2026-05-17 00:32:42 -0700

    fix: remove local cacheDir from Tina client, remove dev admin/index.html so tinacms build generates production version

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,8 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ cacheDir: '/private/tmp/kagnew/tina/__generated__/.cache/1779000995578', url: 'https://content.tinajs.io/2.4/content/d61bf053-244c-4ef8-8ee9-6fa6175fa836/github/main', token: '761a857b34dad1296432c653da5ae35e840a515f', queries,  });
+export const client = createClient({
+  url: 'https://content.tinajs.io/2.4/content/d61bf053-244c-4ef8-8ee9-6fa6175fa836/github/main',
+  token: '761a857b34dad1296432c653da5ae35e840a515f',
+  queries,
+});
 export default client;
-  
